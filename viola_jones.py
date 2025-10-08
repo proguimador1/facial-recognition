a412881f06be684a3f6bd3947cae87870c64d4bf
Guilherme Andrade <guilherme.programmer1@gmail.com>
2025-10-08 09:17:09 -0300

    Implementei uma nova versão do algoritmo de viola-jones

--- viola_jones.py
+++ viola_jones.py
@@ -1,62 +1,36 @@
-import cv2 as cv
-import argparse
-def detectAndDisplay(frame):
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.equalizeHist(frame_gray)
+import cv2
+import os
+cascPathface = os.path.dirname(
+    cv2.__file__) + "/data/haarcascade_frontalface_alt.xml"
+#cascPatheyes = os.path.dirname(
+    #cv2.__file__) + "/data/haarcascade_eye_tree_eyeglasses.xml"
 
-    #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray)
+faceCascade = cv2.CascadeClassifier(cascPathface)
+#eyeCascade = cv2.CascadeClassifier(cascPatheyes)
 
-    for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
-
-        #-- In each face, detect eyes
-        eyes = eyes_cascade.detectMultiScale(faceROI)
-
-        for (x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-
-    cv.imshow('Capture - Face detection', frame)
-
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='haarcascades/haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-args = parser.parse_args()
-face_cascade_name = args.face_cascade
-eyes_cascade_name = args.eyes_cascade
-face_cascade = cv.CascadeClassifier()
-eyes_cascade = cv.CascadeClassifier()
-
-#-- 1. Load the cascades
-if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
-    print('--(!)Error loading face cascade')
-    exit(0)
-
-if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
-    print('--(!)Error loading eyes cascade')
-    exit(0)
-
-#-- 2. Read the video stream
-cap = cv.VideoCapture(0)
-
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
-
+video_capture = cv2.VideoCapture(0)
 while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h),(0,255,0), 2)
+        faceROI = frame[y:y+h,x:x+w]
+        #eyes = eyeCascade.detectMultiScale(faceROI)
+        #for (x2, y2, w2, h2) in eyes:
+            #eye_center = (x + x2 + w2 // 2, y + y2 + h2 // 2)
+            #radius = int(round((w2 + h2) * 0.25))
+            #frame = cv2.circle(frame, eye_center, radius, (255, 0, 0), 4)
 
-    ret, frame = cap.read()
-
-    if frame is None:
-        print('--(!) No captured frame -- Break!')
+        # Display the resulting frame
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 
-    detectAndDisplay(frame)
-
-    if cv.waitKey(1) == ord('q'):
-        break
+video_capture.release()
+cv2.destroyAllWindows()
